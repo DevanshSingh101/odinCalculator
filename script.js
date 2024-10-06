7b3ef01d47c57d28faa48d178f4e08f866d25cda
Devansh Singh <94374878+DevanshSingh101@users.noreply.github.comdsfsdf>
2024-10-06 12:43:37 +0530

    Now the loop is generating numbers

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 const buttonGrid = document.querySelector("#grid");
 const hori_ = document.querySelector('.hori');
 const box_ = document.querySelector('.box');
-let defaultVal = 1;
+let a = 0;
 
 for(let i = 0; i<3; i++){
     // algo = box number = ix + a where a = default and each loop 1 default = i+1
     //this gives the number at a box in order 1 2 3...
-    defaultVal = i + 1; // used as and numbering algorith
-    let a = defaultVal;
+    
+    
     
     let hori = document.createElement('div');    
     hori.className = 'hori';
     buttonGrid.appendChild(hori);
+    let k = 1;
 
 
     for(let x = 1; x <= 3; x++){
+        
         let box = document.createElement('div');
         box.className = 'box';
         let text = document.createElement('p');
         hori.appendChild(box);
         box.appendChild(text);
-        let p = document.querySelector('.box > p');
-        p.innerHTML = i*x + a;
+        // let p = document.querySelector('.box > p');
+        
+        text.innerHTML = a + k;
+        console.log(a+k);
+        k+=1;
 
     }
+
+    a+=3;
 }
